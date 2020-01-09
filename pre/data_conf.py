5751cee67a0f2da280587333b4c9ebb8b17a192e
forrest <forrest.bao@gmail.com>
2020-01-09 03:41:47 -0600

    add mutation; fix bug in cross pairing

--- pre/data_conf.py
+++ pre/data_conf.py
@@ -1,8 +1,19 @@
 dataset_name = "cnn_dailymail"
 splits = ['train', 'validation', 'test']
 special_characters_to_clean = ['\n', '\t', '\''] # replace such strings with spaces in raw data 
-dump_to="'./'+split+'.tsv'" # _split_ is an element of _splits_ above 
+dump_to="'./'+split+method+'.tsv'" # _split_ is an element of _splits_ above 
 take_percent =  1  # range from 0 to 100; 100 means all; 0 means none. 
 features =['article', 'highlights']
 in_memory=False
+sent_end = [".","!","?"]  # symbols that represent the end of a sentence 
 
+
+# parameters for cross pairing
+use_cross = False  # True to enable sample generation using cross pairing; False to disable
+cross_method = ["cross"] 
+
+# parameters for mutate 
+use_mutate = True # True to enable sample generation using mutation; False to disable
+
+mutate_ratios = [10, 30, 50, 70] # percentage
+mutate_method = ["add", "delete", "replace"] # comment this line to disable 
--- pre/data_prepare.py
+++ pre/data_prepare.py
@@ -45,7 +45,7 @@ def cross_pair(data_pairs, dump_to=None, in_memory=False ):
     """Create positive and negative samples using cross pairing 
 
     input:
-        data_pairs: list of 2-tuples (a document, its summary)
+        data_pairs: list of 2-tuples of strings (a document, its summary)
 
         dump_to: str, file path to dump the labeled doc-sum pair
                 default none. 
@@ -56,48 +56,188 @@ def cross_pair(data_pairs, dump_to=None, in_memory=False ):
     return: list of triplets, [doc, sum, 0 or 1]
             0 if sum and doc do not match. 1 if so. 
 
+    example: 
+        >>> cross_pair([("1", "2"),("3", "4")], in_memory=True)
+        [('1', '2', 1), ('3', '4', 1), ('1', '4', 0), ('3','2', 0)]
+
+        >>> cross_pair([("1", "2"),("3", "4"), ("5", "6")], in_memory=True)
+        [('1', '2', 1),
+         ('1', '4', 0),
+         ('1', '6', 0),
+         ('3', '2', 0),
+         ('3', '4', 1),
+         ('3', '6', 0),
+         ('5', '2', 0),
+         ('5', '4', 0),
+         ('5', '6', 1)]
+
     """
 
-    pos_samples, neg_samples = None, None 
+    samples = []
 
     if dump_to != None:
-        with open(dump_to, 'w') as f: 
-            for (_doc, _sum) in data_pairs:
-                f.write("\t".join([_doc, _sum, "1\n"]))
+        f = open(dump_to, 'w')
 
-            for doc_index in range(len(data_pairs)):
-                _doc = data_pairs[doc_index][0]
-                for sum_index in range(doc_index+1, len(data_pairs)):
-                    _sum = data_pairs[sum_index][1]
-                f.write("\t".join([_doc, _sum, "0\n"]))
+    for doc_index in range(len(data_pairs)):
+        _doc = data_pairs[doc_index][0]
+        for sum_index in range(len(data_pairs)):
+            _sum = data_pairs[sum_index][1] 
+            if doc_index == sum_index: 
+                label = 1 
+            else: 
+                label = 0 
+            if dump_to != None:
+                f.write("\t".join([_doc, _sum, str(label)+"\n"]))
+            if in_memory :
+                samples.append((_doc,_sum, label ))
 
-        
-    if in_memory: 
-        pos_samples = [(_doc, _sum, 1) for (_doc, _sum) in data_pairs ]
+    if dump_to != None:
+        f.close()
+    return samples
 
-        neg_samples = [] 
-        for doc_index in range(len(data_pairs)):
-            _doc = data_pairs[doc_index][0]
-            for sum_index in range(doc_index+1, len(data_pairs)):
-                _sum = data_pairs[sum_index][1]
-                neg_samples.append((_doc,_sum, 0 ))
+### functions related to mutation 
 
-    return pos_samples, neg_samples 
+def get_vocab(data_pairs):
+    """generate a set of all vocabularies, tokenized by spaces, from pairs of documents and summaries  
+    """
+    long_doc, long_sum = "", "" 
+    for (_doc, _sum) in data_pairs:
+        long_doc += _doc
+        long_sum += _sum 
+    all_sent = long_doc + long_sum 
+    vocab = set(all_sent.split(' '))
+    return vocab 
 
-def mutate(data_pairs):
-    pass
+def mutate_add(words, vocab, ratio):
+    """add _ratio_% of vocab into random locations in _words_
+
+    words: list of strings, e.g., ["I", "am", "lucky"]
+    vocab: list of strings, 
+    ratio: int, 0 to 100. 
+
+    """
+    ratio /= 100. 
+    length = len(words)
+    indices = set([random.randint(0, length)
+                   for _ in range(int((1 - ratio) * length))])
+    res = []
+    for i in range(length):
+        if i in indices:
+            res.append(vocab[random.randrange(len(vocab))])
+        res.append(words[i])
+    return ' '.join(res)
+
+def mutate_delete(words, ratio, sent_end):
+    """delete _ratio_% of words in random locations of _words_, 
+    while preserving sentence separators
+
+    words: list of strings, e.g., ["I", "am", "lucky"]
+    ratio: int, 0 to 100. 
+    sent_end: list of strings 
+    """
+    ratio /= 100.
+    length = len(words)
+    indices = set(random.sample(range(length),
+                                int((1 - ratio) * length)))
+    return ' '.join([words[i] for i in range(length)
+                     if i not in indices or words[i] in sent_end])
+
+def mutate_replace(words, vocab, ratio, sent_end):
+    """replace _ratio_% of words in random locations of _words_, 
+    while preserving sentence separators
+
+    words: list of strings, e.g., ["I", "am", "lucky"]
+    vocab: list of strings, 
+    ratio: int, 0 to 100. 
+    sent_end: list of strings 
+    """
+
+    ratio /= 100. 
+    length = len(words)
+    indices = set([random.randint(0, length)
+                   for _ in range(int((1 - ratio) * length))])
+    res = []
+    for i in range(length):
+        if i in indices and words[i] not in sent_end:
+            res.append(vocab[random.randrange(len(vocab))] )
+        else:
+            res.append(words[i])
+    return ' '.join(res)
+
+def mutate(data_pairs, ratio, method,  dump_to=None, in_memory=False ):
+    """Create positive and negative samples using cross pairing 
+
+    input:
+        data_pairs: list of 2-tuples of strings (a document, its summary) 
+
+        ratio: int, 0 to 100. The percentage of mutation. 
+
+        method: str, one of ["add", "delete", "replace"]
+
+        dump_to: str, file path to dump the labeled doc-sum pair
+                default none. 
+
+        in_memory: Bool, whether to return labeled samples in memory
+                default False 
+
+    return: list of triplets, [doc, mutated sum, ratio]
+         
+    """
+    import random 
+    all_vocab = get_vocab(data_pairs)
+    mutated = []
+
+    if dump_to != None: 
+        f= open(dump_to, 'w') 
+
+    for (_doc, _sum) in data_pairs:
+        # split the words and then feed to mutator 
+        splitted_summary = _sum.split(' ')
+        if method == "add":
+            mutated_tmp = mutate_add(splitted_summary, all_vocab, ratio)
+        elif  method == "delete":
+            mutated_tmp = mutate_delete(splitted_summary, ratio, sent_end)
+        elif  method == "replace":
+            mutated_tmp = mutate_add(splitted_summary, all_vocab, ratio, sent_end)
+        else: 
+            print ("wrong method of mutation")
+            exit()
+
+        if dump_to != None:
+            f.write("\t".join([_doc, _sum, str(ratio)]))
+            
+        if in_memory: 
+            mutated.append(mutated_tmp)
+
+    if dump_to != None:
+        f.close()
+
+    return mutated 
+
+### functions at highest levels
+
+def lazy_mutated(methods):
+    import data_conf
+    for split in data_conf.splits:
+        pairs = load_pairs(data_conf.dataset_name, split, data_conf.take_percent, data_conf.features, data_conf.special_characters_to_clean)
+        for method in data_conf.methods: 
+            samples = mutate(pairs, data_conf.ratio, method, dump_to=eval(data_conf.dump_to), in_memory = data_conf.in_memory)
+
+
+    return samples # only the last one 
 
 
 def lan_ren_bao():
     import data_conf
+    method = data_conf.cross_method[0] 
 
     for split in data_conf.splits:
         pairs = load_pairs(data_conf.dataset_name, split, data_conf.take_percent, data_conf.features, data_conf.special_characters_to_clean)
-        pos_samples, neg_samples = cross_pair(pairs, dump_to=eval(data_conf.dump_to), in_memory=data_conf.in_memory)
+        samples = cross_pair(pairs, dump_to=eval(data_conf.dump_to), in_memory=data_conf.in_memory)
 
-    return pairs
+    return samples # only the last one
 
 if __name__ == "__main__":
-    pairs = lan_ren_bao()
+    samples = lan_ren_bao()
 
 

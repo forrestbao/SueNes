ea8b12bc8f8b7046805277c601a224acef2eda18
forrest <forrest.bao@gmail.com>
2020-01-11 09:55:56 -0600

    add tf.Session()

--- pre/google_USE_test.py
+++ pre/google_USE_test.py
@@ -9,19 +9,27 @@ sentences = [
     "I am a sentence for which I would like to get its embedding"]
 
 # google encoders 
-google_USE_large = hub.module("https://tfhub.dev/google/universal-sentence-encoder-large/5")
-google_USE_dan = hub.module("https://tfhub.dev/google/universal-sentence-encoder/4")
+google_USE_large = hub.Module("https://tfhub.dev/google/universal-sentence-encoder-large/5")
+google_USE_dan = hub.Module("https://tfhub.dev/google/universal-sentence-encoder/4")
+
+def embed_sents(sent, encoder):
+  """Given a list of sentences, get their embeddings
+  """
+  with tf.Session() as session:      
+    session.run([tf.global_variables_initializer(), tf.tables_initializer()])
+    embeddings = session.run(encoder(sents))  # numpy array
+    print (embeddings)
+    
+embed_one_sent(["Can I use these with Window 8/8.1?"], Google_USE)
 
 from tensorflow.python.client import device_lib
 for dev in device_lib.list_local_devices():
     if dev.device_type in ["CPU", "GPU"]:
         with tf.device(dev.name):
             print ("testing Google USE Transformer/Large on {}".format([dev.name]))
-            google_large_embeddings = google_USE_large(sentences)
-            print (google_large_embeddings)
+            embed_sents(sentences, google_USE_large)
 
             print ("testing Google USE DAN/small on {}".format([dev.name]))
-            google_dan_embeddings = google_USE_dan(sentences)
-            print (google_dan_embeddings)
+            embed_sents(sentences, google_USE_dan)
 
 

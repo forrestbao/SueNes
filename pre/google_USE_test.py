de2eaf1ce8ae68884edb98fca6bfaa31e52a07b7
forrest <forrest.bao@gmail.com>
2020-01-12 01:45:00 -0600

    migrate tf 1 to tf2

--- pre/google_USE_test.py
+++ pre/google_USE_test.py
@@ -11,35 +11,40 @@ sentences = [
 # google encoders 
 
 
-TF_Version = 2 # 1 or 2 
-
-
+TF_Version = int(tf.__version__[0])
 
 if TF_Version == 1:
     google_USE_large = hub.Module("https://tfhub.dev/google/universal-sentence-encoder-large/3")
     google_USE_dan = hub.Module("https://tfhub.dev/google/universal-sentence-encoder/2")
 
+    def embed_sents(sents, encoder):
+      """Given a list of sentences, get their embeddings
+      """
+      with tf.Session() as session:      
+        session.run([tf.global_variables_initializer(), tf.tables_initializer()])
+        embeddings = session.run(encoder(sents))  # numpy array
+        return  (embeddings)
+
 elif TF_Version == 2: 
     google_USE_large = hub.load("https://tfhub.dev/google/universal-sentence-encoder-large/5")
     google_USE_dan = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
-
-def embed_sents(sents, encoder):
-  """Given a list of sentences, get their embeddings
-  """
-  with tf.Session() as session:      
-    session.run([tf.global_variables_initializer(), tf.tables_initializer()])
-    embeddings = session.run(encoder(sents))  # numpy array
-    print (embeddings)
-    
 
 from tensorflow.python.client import device_lib
 for dev in device_lib.list_local_devices():
     if dev.device_type in ["CPU", "GPU"]:
         with tf.device(dev.name):
             print ("testing Google USE Transformer/Large on {}".format([dev.name]))
-            embed_sents(sentences, google_USE_large)
+            if TF_Version == 1:
+                embeddings = embed_sents(sentences, google_USE_large)
+            elif TF_Version == 2: 
+                embeddings = google_USE_large(sentences)
+            print (embeddings) 
 
             print ("testing Google USE DAN/small on {}".format([dev.name]))
-            embed_sents(sentences, google_USE_dan)
+            if TF_Version == 1:
+                embeddings = embed_sents(sentences, google_USE_dan)
+            elif TF_Version == 2: 
+                embeddings = google_USE_dan(sentences)
+            print (embeddings) 
 
 

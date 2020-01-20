458faa7f36ad479e1dedb946965b8f1f4a9f07b9
forrest <forrest.bao@gmail.com>
2020-01-20 03:22:42 -0600

    allow shuffling and shard for data loaded via TFDS

--- pre/sample_conf.py
+++ pre/sample_conf.py
@@ -1,28 +1,43 @@
-#======== data loading parameters 
-
-load_from = "tsv" # "tfds" or "tsv"
-
-# Whether save the data loaded in TFDS to TSV. 
-# If load_from is tsv, no effect  
-save_tsv = True
-
-# Must match their names in TFDS 
-dataset_name = "cnn_dailymail"
+# ==== constants about datasets 
 dataset_features = {"cnn_dailymail": ['article', 'highlights'],
     "big_patent": ['description', 'abstract'],
     "newsroom": ['text', 'summary'],
-    "scientific_papers": ['artcile', 'abstract'],
+    "scientific_papers": ['article', 'abstract'],
     "billsum":['text','summary']
     }
 
 dataset_sizes = {"billsum":23455, "scientific_papers":215913, "newsroom":1212740, 
         "cnn_dailymail":311971, "big_patent":1341362}
 
-splits = ['train', 'validation', 'test']
+#======== data loading parameters 
+
+load_from = "tfds" # "tfds" or "tsv"
+
+# Whether save the data loaded in TFDS to TSV. 
+# If load_from is tsv, no effect  
+save_tsv = True
+
+# should we scramble the data
+# only effective when load_from is tfds 
+scramble = True 
+
+# Must match their names in TFDS 
+dataset_name = "scientific_papers"
+
 features = dataset_features[dataset_name]
 
-# Percentage of data from the split to be used to generate samples
-take_percent =  1  # range from 0 to 100; 100 means all;0 means none. 
+splits = ['train', 'test', 'validation']
+# note that billsum has no validation set
+
+# Percentage of data to load from orignal size 
+load_percent =  1  # range from 0 to 100; 100 means all;0 means none. 
+
+# Amoung loaded, usually after scambling, use only 1/num_shards of this dataset.
+# Not to confuse with take_percent.
+# for example, we may load all data and only use 1/10 of it, 
+# vs. load only the first 10 percent and use all of that 10 percent. 
+num_shards = 10 
+
 
 #========= data output/dumping parameters 
 

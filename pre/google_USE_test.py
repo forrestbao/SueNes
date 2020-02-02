40bace8b508c79fafabd2e72aaf92fb00590ee01
Forrest Bao <forrest.bao@gmail.com>
2020-02-02 22:10:30 +0800

    update google USE test for manually downloaded tfhub modules

--- pre/google_USE_test.py
+++ pre/google_USE_test.py
@@ -1,15 +1,15 @@
 # for testing sentence encoders on your GPU environment
 
-
 import tensorflow_hub as hub
 import tensorflow as tf
-import time
+import time, os 
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2" 
 
 sentences = [
     "The quick brown fox jumps over the lazy dog.",
     "I am a sentence for which I would like to get its embedding"]* int(1*2**15)
 DAN_scale_factor=4
-
 
 print (tf.__version__)
 TF_Version = int(tf.__version__[0])
@@ -29,15 +29,16 @@ if TF_Version == 1:
         return  (embeddings)
 
 elif TF_Version == 2: 
-    google_USE_large = hub.load("https://tfhub.dev/google/universal-sentence-encoder-large/5")
-    google_USE_dan = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
+        # google_USE_large = hub.load("https://tfhub.dev/google/universal-sentence-encoder-large/5")
+        # google_USE_dan = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
+        google_USE_large = hub.load("/work/data/tfhub/google_use_large_5") 
+        google_USE_dan = hub.load("/work/data/tfhub/google_use_dan_4")
 
 #from tensorflow.python.client import device_lib
 #for dev in device_lib.list_local_devices():
 #    if dev.device_type in ["CPU", "GPU"]:
 #        with tf.device(dev.name):
 #print ("testing Google USE Transformer/Large on {}".format([dev.name]))
-
 
 t=time.time()
 print ("testing Google USE Transformer/Large")
@@ -49,7 +50,7 @@ print (embeddings)
 print ("elapsed seconds {}".format(time.time()-t))
 
 t=time.time()
-print ("testing Google USE DAN/small on")
+print ("testing Google USE DAN/small")
 if TF_Version == 1:
     embeddings = embed_sents(sentences*DAN_scale_factor, google_USE_dan)
 elif TF_Version == 2: 

4a8d0e91a2edf6c2e297314de03f98251b7d579f
Hebi Li <hebi@lihebi.com>
2020-12-14 15:53:16 +0800

    remove backup folder

--- backup/utils/parse_result.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-def test():
-    with open('log.txt') as f:
-        for line in f:
-            if line.startswith('SETTING:'):
-                print(line)
-            if line.startswith('RESULT:'):
-                print(line)

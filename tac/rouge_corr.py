9cb277630f67a4d79e3ea91ab5496ca8eb06ac03
nkwbtb <nkwbtb@outlook.com>
2021-08-30 18:07:55 -0500

    add system level & summary level

--- tac/rouge_corr.py
+++ tac/rouge_corr.py
@@ -1,23 +1,63 @@
 from tac import *
 import numpy as np
-from scipy.stats.stats import pearsonr, spearmanr
+from scipy.stats.stats import pearsonr, spearmanr, kendalltau
 import json
 
-def calc_corr(metric_score, scores, rscore_type, hscore_type):
-    x = [ [metric_score[key][i] for key in rouge_scores.keys()] for i in range(rscore_type)]
-    y = [ [scores[doc][summarizer][i] for doc, summarizer in rouge_scores.keys()] for i in range(hscore_type)]
+def calc_corr(metric_score, scores, rscore_type, hscore_type, method=pearsonr, level="pool"):
+    """ Calculate correlation between metrics and human evaluation score
+        
+        metric_score: dict, scores for metrics
+        scores: dict, scores for human evaluation   
+        rscore_type: int, the number of metrics
+        hscore_type: int, the number of categories in human evaluation
+        methods: function, the correlation methods
+        level: str, the level used for calculation, can be "pool" or "summary" or "system"
 
-    corr_pearson = np.zeros((rscore_type, hscore_type), dtype=np.float32)
-    corr_spearman = np.zeros((rscore_type, hscore_type), dtype=np.float32)
-    for i in range(rscore_type):
-        for j in range(hscore_type):
-            corr_pearson[i, j] = pearsonr(x[i], y[j])[0]
-            corr_spearman[i, j] = spearmanr(x[i], y[j])[0]
+        return: numpy array of shape (rscore_type, hscore_type)
+    """
+    corr = np.zeros((rscore_type, hscore_type), dtype=np.float32)
+    if level == 'pool':
+        x = [ [metric_score[key][i] for key in metric_score.keys()] for i in range(rscore_type)]
+        y = [ [scores[doc][summarizer][i] for doc, summarizer in metric_score.keys()] for i in range(hscore_type)]
+        
+        for i in range(rscore_type):
+            for j in range(hscore_type):
+                corr[i, j] = method(x[i], y[j])[0]
+    elif level == 'summary':
+        for i in range(rscore_type):
+            for j in range(hscore_type):
+                for doc in scores:
+                    x = [metric_score[(doc, summarizer)][i] for summarizer in scores[doc] if summarizer.isnumeric()]
+                    y = [scores[doc][summarizer][j] for summarizer in scores[doc] if summarizer.isnumeric()]
+                    # Caculate Correlation for each of the document
+                    corr[i, j] += method(x, y)[0]
+        
+        # Mean value across docs
+        corr /= len(scores)
+    
+    elif level == 'system':
+        for i in range(rscore_type):
+            for j in range(hscore_type):
+                x_all = []
+                y_all = []
+                for doc in scores:
+                    x_all.append([metric_score[(doc, summarizer)][i] for summarizer in scores[doc] if summarizer.isnumeric()])
+                    y_all.append([scores[doc][summarizer][j] for summarizer in scores[doc] if summarizer.isnumeric()])
+                x_all = np.array(x_all)
+                y_all = np.array(y_all)
+                
+                # Aggregate score over documents by mean
+                assert(x_all.shape == y_all.shape)
+                x = np.mean(x_all, axis=0)
+                y = np.mean(y_all, axis=0)
+                corr[i, j] += method(x, y)[0]
+    else:
+        print("???")
+        assert(False)
 
-    output = np.concatenate((corr_pearson, corr_spearman), axis=1)
-    return output
+    return corr
 
-if __name__ == "__main__":  
+def main():
     TAC_result_root = "/home/nkwbtb/Downloads/TAC2010"
     score_path = TAC_result_root + "/GuidedSumm2010_eval/manual"
     rouge_score_path = TAC_result_root + "/GuidedSumm2010_eval/ROUGE/rouge_A.m.out"
@@ -25,7 +65,6 @@ if __name__ == "__main__":
     output_file = "rouge_score.tsv"
 
     setIDs = ["A"]
-    sentence_delimiter = "  "
     summary_types = ["peers", "models"]
 
     hscore_type = 3
@@ -33,8 +72,11 @@ if __name__ == "__main__":
     
     scores = get_scores(score_path, summary_types, setIDs)
     rouge_scores = get_rouge(rouge_score_path)
+
+    method = kendalltau
+    level = 'summary'
     
-    output = calc_corr(rouge_scores, scores, rscore_type, hscore_type)
+    output = calc_corr(rouge_scores, scores, rscore_type, hscore_type, method, level)
 
     with open(output_file, "w", encoding="UTF-8") as f:
         for line in output:
@@ -62,10 +104,13 @@ if __name__ == "__main__":
         summarizer = doc_string[-1]
         converted_score[(doc, summarizer)] = score_list
     
-    output = calc_corr(converted_score, scores, rscore_type, hscore_type)
+    output = calc_corr(converted_score, scores, rscore_type, hscore_type, method, level)
     with open("baselines_score.tsv", "w", encoding="UTF-8") as f:
         lc = 0
         for line in output:
             strline = score_type[lc] + "\t" + "\t".join([str(val) for val in line]) + "\n"
             f.write(strline)
             lc += 1
+
+if __name__ == "__main__":  
+    main()
